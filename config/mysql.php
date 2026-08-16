17d3bb59f25bf216ed6fd2125e58b05e63bbfef8
Erica Donjo <ericadonjo2006@gmail.com>
2026-08-16 20:41:38 +0100

    Ajout du fichier render.yaml

--- config/mysql.php
+++ config/mysql.php
@@ -1,8 +1,8 @@
 <?php
 
-const MYSQL_HOST = '127.0.0.1';
+const MYSQL_HOST = 'sql204.infinityfree.com';
 const MYSQL_PORT = 3306;
-const MYSQL_DATABASE = 'site_ecole';
-const MYSQL_USER = 'root';
-const MYSQL_PASSWORD = '';
+const MYSQL_DATABASE = 'if0_42671264_site_ecole';
+const MYSQL_USER = 'if0_42671264';
+const MYSQL_PASSWORD = 'djvIvtUZso5';
 ?>
